08f844485abfd493f5ac001e1f0b8dcafa7b9e4f
ivana092 <ivanapathak8898@gmail.com>
2018-12-19 16:35:17 +0530

    Updates

---
+++ precache-manifest.c43c0c992ace3ecbd70aee32510238ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "9c73aa960746956b4049",
+    "url": "/robofriends/static/js/main.9c73aa96.chunk.js"
+  },
+  {
+    "revision": "f34b2b9d1f1c1a411b17",
+    "url": "/robofriends/static/js/1.f34b2b9d.chunk.js"
+  },
+  {
+    "revision": "9c73aa960746956b4049",
+    "url": "/robofriends/static/css/main.d91625d5.chunk.css"
+  },
+  {
+    "revision": "f34b2b9d1f1c1a411b17",
+    "url": "/robofriends/static/css/1.525055ef.chunk.css"
+  },
+  {
+    "revision": "c19112e57ca9e3e0bae0a8ee6d11c45f",
+    "url": "/robofriends/index.html"
+  }
+];
